c42d6343d45607a41b3499763426e3b3d56120ba
Rafee <rafeehasan88@gmail.com>
2025-01-20 11:24:36 +0600

    nested route and navbar setup

---
+++ src/Components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h2 className='text-5xl text-center'>Page not found!  Status- 404</h2>
+        </div>
+    );
+};
+
+export default ErrorPage;
